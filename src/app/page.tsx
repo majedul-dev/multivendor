2df5b8f8244d6febd3dd36b9c77145f477739fe5
Majedul Islam <majedulislam956@gmail.com>
2025-05-08 21:13:59 +0000

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Add More</Button>
+      <p className="text-rose-500">Hellow World!</p>
+    </div>
+  );
+}
