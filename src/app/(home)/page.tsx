7f2d89b72cd1fd7210f7847d5a8406873e36b299
Majedul Islam <majedulislam956@gmail.com>
2025-05-09 01:03:27 +0000

    03: home layout - removed unused imports

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
-
 export default function Home() {
   return (
     <div className="p-4">
-      
+      Home page
     </div>
   );
 }
