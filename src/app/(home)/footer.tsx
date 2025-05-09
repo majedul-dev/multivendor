e69d245086c3b0d5692c7a6a98081cdb0415b5eb
Majedul Islam <majedulislam956@gmail.com>
2025-05-09 00:56:17 +0000

    03: home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex items-center">
+        <p>proSHOP, inc</p>
+      </div>
+    </footer>
+  )
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      
+    </div>
+  );
+}
